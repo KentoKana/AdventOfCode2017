e725bcaf50d3f1618c8e6afcaed0004cead381d5
Kento K <33736354+KentoKana@users.noreply.github.com>
2018-02-22 01:27:48 -0800

    Add files via upload

---
+++ D2/Day2Part2.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const text = fs.readFileSync("./D2_input.txt").toString('utf-8');
+let stringArray = text.split(/[\n]/);
+
+//converts data into array of integers
+toInt=()=>{
+	let array = [];
+	for(i=0;i<stringArray.length;i++){
+		array[i] = stringArray[i].split(/[\r\t]/);
+		array[i] = array[i].filter(Boolean);
+		for(j=0;j<stringArray.length;j++){
+			array[i][j] = parseInt(array[i][j]);
+		}
+	}
+	return array;
+}
+var numArray = toInt();
+
+
+
+checkSum =someArray=>{
+	var counter = 0;
+	for(let i=0;i<someArray.length;i++){
+		for(let j=0;j<someArray[i].length;j++){
+			for(let k=0;k<someArray[i].length;k++){
+
+				//avoids comparison between the same array item.
+				//assumption is that no two numbers are the same in a single list.
+				if(someArray[i][j] === someArray[i][k]){
+					continue;
+
+				//Checks if any combination of the items would return a whole number
+				} else if(someArray[i][j]%someArray[i][k] === 0){
+
+					//checks whether the quotient between two items is a float.
+					//If it is, flip the denominator and the numerator to get a whole number as a quotient.
+					if((someArray[i][j]/someArray[i][k])%1 === 0){
+						counter += someArray[i][j]/someArray[i][k];
+					} else{
+						counter += someArray[i][k]/someArray[i][j];
+					}
+				}
+			}
+		}
+	}
+	return counter;
+}
+
+console.log(checkSum(numArray));
